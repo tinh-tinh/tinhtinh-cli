726a4fefcd7531e85e07828724d1f11beca2d687
Ren0503 <illuminati0440@gmail.com>
2024-10-22 20:21:16 +0700

    feat: sync up v1.2.0

--- tpl/main.go
+++ tpl/main.go
@@ -62,24 +62,24 @@ import "github.com/tinh-tinh/tinhtinh/core"
 func NewController(module *core.DynamicModule) *core.DynamicController {
 	ctrl := module.NewController("{{ .ModName }}")
 
-		ctrl.Post("/", func(ctx core.Ctx) {
-		ctx.JSON(core.Map{"data": "ok"})
+		ctrl.Post("/", func(ctx core.Ctx) error {
+		return ctx.JSON(core.Map{"data": "ok"})
 	})
 
-	ctrl.Get("/", func(ctx core.Ctx) {
-		ctx.JSON(core.Map{"data": "ok"})
+	ctrl.Get("/", func(ctx core.Ctx) error {
+		return ctx.JSON(core.Map{"data": "ok"})
 	})
 
-	ctrl.Get("/{id}", func(ctx core.Ctx) {
-		ctx.JSON(core.Map{"data": "ok"})
+	ctrl.Get("/{id}", func(ctx core.Ctx) error {
+		return ctx.JSON(core.Map{"data": "ok"})
 	})
 
-	ctrl.Put("/{id}", func(ctx core.Ctx) {
-		ctx.JSON(core.Map{"data": "ok"})
+	ctrl.Put("/{id}", func(ctx core.Ctx) error {
+		return ctx.JSON(core.Map{"data": "ok"})
 	})
 
-	ctrl.Delete("/{id}", func(ctx core.Ctx) {
-		ctx.JSON(core.Map{"data": "ok"})
+	ctrl.Delete("/{id}", func(ctx core.Ctx) error {
+		return ctx.JSON(core.Map{"data": "ok"})
 	})
 
 	return ctrl
