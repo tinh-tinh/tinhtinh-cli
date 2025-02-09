e218cee956bf4a42a6ae8bd83e29c610b7a5bb73
Ren0503 <illuminati0440@gmail.com>
2025-02-09 20:15:35 +0700

    feat: upgrade v2

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/tinh-tinh/tinhtinh-cli
+module github.com/tinh-tinh/tinhtinh-cli/v2
 
 go 1.22.0
 
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/tinh-tinh/tinhtinh-cli/cmd"
+import "github.com/tinh-tinh/tinhtinh-cli/v2/cmd"
 
 func main() {
 	cmd.Execute()
